1e47b82f2374f1620fea3eeb0818bf20de77c36f
Keyur Shah <51239095+keyurboss@users.noreply.github.com>
2025-12-31 01:29:39 +0530

    fix: Golang and Build Script Added and CI Added

--- linux.sh
+++ linux.sh
@@ -1,3 +1,13 @@
-GOOS=linux GOARCH=amd64 go build -o "http-dump.o" -ldflags="-s -w" -v ./servers/http-dump/dump-server/main.go
-GOOS=linux GOARCH=amd64 go build -o "message-dump.o" -ldflags="-s -w" -v ./servers/http-dump/message-dump/main.go
-GOOS=linux GOARCH=amd64 go build -o "telegram-server.o" -ldflags="-s -w" -v ./servers/telegram-server/main.go
+GOOS=linux GOARCH=amd64 go build ./...
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/http-dump.o" -ldflags="-s -w" -v ./servers/http-dump/dump-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/message-dump.o" -ldflags="-s -w" -v ./servers/http-dump/message-dump/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/telegram-server.o" -ldflags="-s -w" -v ./servers/telegram-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/boozv3.o" -ldflags="-s -w" -v ./servers/boozv3/main-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/bullion.o" -ldflags="-s -w" -v ./servers/bullion/main-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/function-management.o" -ldflags="-s -w" -v ./servers/function-management/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/link-shortner.o" -ldflags="-s -w" -v ./servers/link-shortner/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/whatsapp-server.o" -ldflags="-s -w" -v ./servers/whatsapp-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/mysql-to-surreal.o" -ldflags "-s -w" -v ./servers/jwelly/mysql-to-surreal/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/main-server.o" -ldflags "-s -w" -v ./servers/jwelly/main-server/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/mysql-backup.o" -ldflags "-s -w" -v ./servers/jwelly/mysql-backup/main.go
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64/mysql-to-mysql.o" -ldflags "-s -w" -v ./servers/jwelly/mysql-to-mysql/main.go
---
+++ windwos.sh
@@ -0,0 +1,13 @@
+GOOS=windows GOARCH=amd64 go build -v -ldflags="-s -w" ./...
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/http-dump.exe" -v -ldflags="-s -w" ./servers/http-dump/dump-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/message-dump.exe" -v -ldflags="-s -w" ./servers/http-dump/message-dump/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/telegram-server.exe" -v -ldflags="-s -w" ./servers/telegram-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/boozv3.exe" -v -ldflags="-s -w" ./servers/boozv3/main-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/bullion.exe" -v -ldflags="-s -w" ./servers/bullion/main-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/function-management.exe" -v -ldflags="-s -w" ./servers/function-management/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/link-shortner.exe" -v -ldflags="-s -w" ./servers/link-shortner/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/whatsapp-server.exe" -v -ldflags="-s -w" ./servers/whatsapp-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/mysql-to-surreal.exe" -v -ldflags="-s -w" ./servers/jwelly/mysql-to-surreal/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/main-server.exe" -v -ldflags="-s -w" ./servers/jwelly/main-server/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/mysql-backup.exe" -v -ldflags="-s -w" ./servers/jwelly/mysql-backup/main.go
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64/mysql-to-mysql.exe" -v -ldflags="-s -w" ./servers/jwelly/mysql-to-mysql/main.go
