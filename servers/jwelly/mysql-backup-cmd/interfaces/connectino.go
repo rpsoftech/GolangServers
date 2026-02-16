1ca4c19a04e90b811cf43fe5affe2be5ba9acc16
Keyur Shah <keyurshah3939@gmail.com>
2026-02-16 15:26:27 +0530

    WIP

---
+++ servers/jwelly/mysql-backup-cmd/interfaces/connectino.go
@@ -0,0 +1,52 @@
+package mysql_backup_interfaces
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	coreEnv "github.com/rpsoftech/golang-servers/env"
+	env "github.com/rpsoftech/golang-servers/servers/jwelly/mysql-backup-cmd/env"
+	utility_functions "github.com/rpsoftech/golang-servers/utility/functions"
+	mysqldb "github.com/rpsoftech/golang-servers/utility/mysql"
+)
+
+// "root:rootpasswd@tcp(localhost:3306)/dbname?charset=utf8mb4&parseTime=true"
+const dsn = "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true"
+
+type ConfigWithConnection struct {
+	ServerConfig      *env.ServerConfig
+	ConnectionString  string
+	BaseDir           string
+	MysqlDbConncetion *mysqldb.MysqlDBStruct
+	SFileServerConfig IFileServerConfigInterface
+}
+
+func ValidateAllConnectionsAndAssign(c *ConfigWithConnection) error {
+	if mysqlDb, err := mysqldb.InitalizeMysqlDbWithConfig(&c.ServerConfig.MysqlConfig); err != nil {
+		return err
+	} else {
+		// mysqlDb.Db.Close()
+		c.BaseDir = filepath.Join(coreEnv.FindAndReturnCurrentDir(), "backup", c.ServerConfig.Name)
+		if exists, err := utility_functions.Exist(c.BaseDir); !exists && err == nil {
+			os.MkdirAll(c.BaseDir, 0777)
+		} else if err != nil {
+			println(err.Error())
+		}
+		c.ConnectionString = fmt.Sprintf(dsn, c.ServerConfig.MysqlConfig.MYSQL_USERNAME, c.ServerConfig.MysqlConfig.MYSQL_PASSWORD, c.ServerConfig.MysqlConfig.MYSQL_HOST, c.ServerConfig.MysqlConfig.MYSQL_PORT, c.ServerConfig.MysqlConfig.MYSQL_DATABASE)
+		c.MysqlDbConncetion = mysqlDb
+		if c.ServerConfig.FileServerType == "TYPE1" {
+			c.SFileServerConfig = &SFileServerType1{
+				FolderPath: c.ServerConfig.FileServerConfig["folderPath"],
+				SFileServerConfig: &SFileServerConfig{
+					URL:   c.ServerConfig.FileServerConfig["url"],
+					TOKEN: c.ServerConfig.FileServerConfig["token"],
+				},
+			}
+			if _, err := c.SFileServerConfig.Validate(); err != nil {
+				panic(err)
+			}
+		}
+	}
+	return nil
+}
--- servers/jwelly/mysql-backup-cmd/main.go
+++ servers/jwelly/mysql-backup-cmd/main.go
@@ -1,17 +1,93 @@
-package mysqlbackupcmd
+package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/robfig/cron/v3"
+	coreEnv "github.com/rpsoftech/golang-servers/env"
+	env "github.com/rpsoftech/golang-servers/servers/jwelly/mysql-backup-cmd/env"
+	interfaces "github.com/rpsoftech/golang-servers/servers/jwelly/mysql-backup-cmd/interfaces"
 )
 
-func main() {
-	// print all command line arguments
-	for _, arg := range os.Args {
-		fmt.Println(arg)
+var DeferFunctionSlice []func() = []func(){}
+var CRON *cron.Cron
+
+func deferFunc() {
+	println("deferFunc")
+	for _, v := range DeferFunctionSlice {
+		v()
 	}
-	// println("Hello World")
-	// wait for user input before exiting
-	fmt.Println("Press Enter to continue...")
-	fmt.Scanln()
+}
+
+func main() {
+	// env.LoadEnv("telegram-server.env")
+	CRON = cron.New()
+	for _, v := range env.ConnectionConfig.ServerConfig {
+		cccc := &interfaces.ConfigWithConnection{ServerConfig: &v}
+		if err := interfaces.ValidateAllConnectionsAndAssign(cccc); err != nil {
+			fmt.Printf("Error In Validating Connectino %s", v.Name)
+			println(err.Error())
+		}
+		if env.ServerEnv.IsDev && env.ServerEnv.Env.APP_ENV == coreEnv.APP_ENV_LOCAL {
+			DoBackupAndUpload(cccc)
+			os.Exit(0)
+		} else {
+			CRON.AddFunc(v.Cron, func() {
+				DoBackupAndUpload(cccc)
+			})
+		}
+	}
+	CRON.Start()
+	// defer deferFunc()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	deferFunc()
+	os.Exit(1)
+}
+
+func DoBackupAndUpload(c *interfaces.ConfigWithConnection) {
+	timeStamoForFileName := time.Now().Unix()
+	filename := fmt.Sprintf("%s-%d.sql.gz", c.ServerConfig.Name, timeStamoForFileName)
+	f, _ := os.Create(filepath.Join(c.BaseDir, filename))
+
+	fmt.Printf("Starting backup for %s at %s \n", c.ServerConfig.Name, time.Now().Format(time.RFC3339))
+	gzipWriter := gzip.NewWriter(f)
+	gzipWriter.Write([]byte("SET foreign_key_checks = 0;\n"))
+	var err error
+	// --add-drop-table --no-data --single-transaction
+	cmd := exec.Command("mysqldump",
+		"-h", c.ServerConfig.MysqlConfig.MYSQL_HOST,
+		"-u", c.ServerConfig.MysqlConfig.MYSQL_USERNAME,
+		"-p"+c.ServerConfig.MysqlConfig.MYSQL_PASSWORD,
+		"--add-drop-table",
+		"--single-transaction",
+		c.ServerConfig.MysqlConfig.MYSQL_DATABASE)
+	cmd.Stdout = gzipWriter
+	err = cmd.Run()
+	gzipWriter.Write([]byte("SET foreign_key_checks = 1;\n"))
+	if err != nil {
+		println(err.Error())
+	}
+	fmt.Printf("backup for %s at %s Completed\n", c.ServerConfig.Name, time.Now().Format(time.RFC3339))
+	gzipWriter.Close()
+	f.Close()
+	if err != nil {
+		println(err.Error())
+	}
+	f, _ = os.Open(filepath.Join(c.BaseDir, filename))
+	defer f.Close()
+	fmt.Printf("Uploading Backup File for %s at %s \n", c.ServerConfig.Name, time.Now().Format(time.RFC3339))
+	c.SFileServerConfig.Upload(f, c, c.ServerConfig.Name)
+	if err != nil {
+		println(err.Error())
+	}
 }
